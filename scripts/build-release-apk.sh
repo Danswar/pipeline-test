24af72772e39f364d31cb890a30287c107a4880a
Daniel Padrino <48102227+Danswar@users.noreply.github.com>
2024-07-23 14:34:05 -0300

    Update build-release-apk.sh

--- scripts/build-release-apk.sh
+++ scripts/build-release-apk.sh
@@ -9,13 +9,13 @@
 echo $KEYSTORE_FILE_HEX > bluewallet-release-key.keystore.hex
 xxd -plain -revert bluewallet-release-key.keystore.hex > ./android/bluewallet-release-key.keystore
 rm bluewallet-release-key.keystore.hex
-ls ./android
 
 cd android
 TIMESTAMP=$(date +%s)
 sed -i'.original'  "s/versionCode 1/versionCode $TIMESTAMP/g" app/build.gradle
-./gradlew assembleRelease
+echo "##### 123 ###"
+./gradlew assembleRelease --stacktrace
+echo "##### 456 ###"
 mv ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk
-ls $ANDROID_HOME/build-tools/33.0.2
 $ANDROID_HOME/build-tools/33.0.2/apksigner sign --ks ./bluewallet-release-key.keystore   --ks-pass=pass:$KEYSTORE_PASSWORD ./app/build/outputs/apk/release/app-release.apk
 
