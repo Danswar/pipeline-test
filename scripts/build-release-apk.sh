936e994da8c31bc3414d0ede7dd26c0e65f51936
Daniel Padrino <danswarrior1@gmail.com>
2024-07-28 21:43:17 -0300

    app bundle

--- scripts/build-release-apk.sh
+++ scripts/build-release-apk.sh
@@ -23,5 +23,6 @@ cd android
 TIMESTAMP=$(date +%s | sed 's/...$//')
 sed -i'.original'  "s/versionCode 1/versionCode $TIMESTAMP/g" app/build.gradle
 ./gradlew assembleRelease -P MYAPP_UPLOAD_STORE_FILE=./bluewallet-release-key.keystore -P MYAPP_UPLOAD_KEY_ALIAS=$KEYSTORE_ALIAS -P MYAPP_UPLOAD_STORE_PASSWORD=$KEYSTORE_PASSWORD -P MYAPP_UPLOAD_KEY_PASSWORD=$KEYSTORE_KEY_PASSWORD
+./gradlew bundleRelease -P MYAPP_UPLOAD_STORE_FILE=./bluewallet-release-key.keystore -P MYAPP_UPLOAD_KEY_ALIAS=$KEYSTORE_ALIAS -P MYAPP_UPLOAD_STORE_PASSWORD=$KEYSTORE_PASSWORD -P MYAPP_UPLOAD_KEY_PASSWORD=$KEYSTORE_KEY_PASSWORD
 $ANDROID_HOME/build-tools/33.0.2/apksigner sign --ks ./bluewallet-release-key.keystore   --ks-pass=pass:$KEYSTORE_PASSWORD ./app/build/outputs/apk/release/app-release.apk
 
